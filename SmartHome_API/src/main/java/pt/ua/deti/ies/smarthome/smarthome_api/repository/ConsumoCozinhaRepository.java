3229e83c8b78f08ebc7a0f3c54a77b9032fd5a5c
Daniel Carvalho <dl.carvalho@ua.pt>
2022-12-10 23:01:56 +0000

    added last 7 days and last 30 days energy consumption API

--- SmartHome_API/src/main/java/pt/ua/deti/ies/smarthome/smarthome_api/repository/ConsumoCozinhaRepository.java
+++ SmartHome_API/src/main/java/pt/ua/deti/ies/smarthome/smarthome_api/repository/ConsumoCozinhaRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pt.ua.deti.ies.smarthome.smarthome_api.model.measurements.ConsumoCozinha;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
 public interface ConsumoCozinhaRepository extends JpaRepository<ConsumoCozinha, Integer> {
+    List<ConsumoCozinha> findAllByDiaEquals(Date dia);
 }
--- SmartHome_API/src/main/java/pt/ua/deti/ies/smarthome/smarthome_api/repository/ConsumoQuartoRepository.java
+++ SmartHome_API/src/main/java/pt/ua/deti/ies/smarthome/smarthome_api/repository/ConsumoQuartoRepository.java
@@ -4,6 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pt.ua.deti.ies.smarthome.smarthome_api.model.measurements.ConsumoQuarto;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
 public interface ConsumoQuartoRepository extends JpaRepository<ConsumoQuarto, Integer> {
+    List<ConsumoQuarto> findAllByDiaEquals(Date dia);
 }
