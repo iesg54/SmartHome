6b421e409d12e5953d570e3bb99dc0abb385020d
afartur <art.afo@ua.pt>
2022-12-30 19:43:30 +0000

    websockets server

---
+++ generators/server.py
@@ -0,0 +1,24 @@
+import asyncio
+import websockets
+import json
+
+CONNECTIONS = {}
+
+async def handler(websocket):
+    async for message in websocket:
+        event = json.loads(message)
+        print(f"Received: {event}")
+
+        if event["type"] == "alert":
+            websocket = CONNECTIONS.get("react")
+            if websocket:
+                await websocket.send(json.dumps(event))
+        elif event["type"] == "react":
+            CONNECTIONS["react"] = websocket
+
+async def main():
+    async with websockets.serve(handler, "localhost", 8765):
+        await asyncio.Future()  # run forever
+
+if __name__ == "__main__":
+    asyncio.run(main())
