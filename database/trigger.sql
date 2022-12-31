df6e22191306b0ccb7dcd71a736a0e714417b238
afartur <art.afo@ua.pt>
2022-12-31 01:14:03 +0000

    dockerfiles

--- database/trigger.sql
+++
@@ -1,9 +0,0 @@
-create trigger sensor_trigger after insert
-on divisao for each row
-begin
-    insert into sensors(generator_type, division_id) values (1, NEW.id);
-    insert into sensors(generator_type, division_id) values (2, NEW.id);
-end;
-
-
-INSERT INTO divisao(nome, id_casa, tipo) values ("teste", 1, "SALA");
